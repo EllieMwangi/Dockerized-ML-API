d522c1b38a8d867ef11cea824c72b8020bea294d
EllieMwangi <emwangi856@gmail.com>
2022-08-26 17:43:48 +0300

    Modify docker and kubernetes files

--- Dockerfile
+++ Dockerfile
@@ -1,12 +1,12 @@
 FROM python:3.7.3-stretch
  
-COPY requirements.txt /app/
-RUN pip install --no-cache-dir --requirement /app/requirements.txt
-
 WORKDIR /app
-COPY . /app/
-# hadolint ignore=DL3013
 
+COPY . /app/
+
+RUN pip install --no-cache-dir --requirement requirements.txt
+
+#hadolint ignore=DL3013
 # Expose port 80
 EXPOSE 80
 
--- run_kubernetes.sh
+++ run_kubernetes.sh
@@ -4,15 +4,16 @@
 
 # Step 1:
 # This is your Docker ID/path
-# dockerpath=<>
+dockerpath=elliemwangi/microservice-api
 
 # Step 2
 # Run the Docker Hub container with kubernetes
-
+kubectl run api-app --image=$dockerpath
 
 # Step 3:
 # List kubernetes pods
+kubectl get pods
 
 # Step 4:
 # Forward the container port to a host
-
+kubectl port-forward pod/api-app 8000:80
